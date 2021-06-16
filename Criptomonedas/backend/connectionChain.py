78e5ec6c2bcf8ca3a4dd3958803f5fe72fc334f5
LucioB16 <luciobotacin@gmail.com>
2021-06-16 03:10:24 -0300

    Add: grafico de velas

--- Criptomonedas/backend/connectionChain.py
+++ Criptomonedas/backend/connectionChain.py
@@ -2,5 +2,7 @@ cosumer_key = "JZX6gCfgOotlNuF27BGaCDQBQ"
 consumer_secret = "PkQn6dkeolEYNCc4xtflnfgapUZdARLIthQq0P1XMI6D84iYS5"
 access_token = "197655488-rFbgLyigCGZZl5lB6AQCbyBL8SAt0OZQdqgn9zr2"
 access_token_secret = "2Qmukr0WycZbLKUz100kMtma7fZZ3kVEe2tRohXe6GZrU"
+binance_key = 'CaPGaOk1BM1qxo1PDEGdTqBD3gCNzobVdZyhtPbbvaWxD9Q1P4zwRQThlZrjnnZw'
+binance_secret = 'hBnO6yz6MtL2iJ36W7HiYLJwssegZwFwvCDsa43dIi0qjLPB7gZBd4S0YfuIhMJU'
 
 #__all__ = ['cosumer_key','consumer_secret','access_token','access_token_secret']
